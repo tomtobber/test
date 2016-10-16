e3d7175e717fc97020d8633cd3b9000df8e5f9ae
Wim Hein Voorsluis <Wim Hein Voorsluis>
2016-10-16 18:30:43 +0200

    url

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var http = require('http');
+var fs = require('fs');
+
+var file = fs.createWriteStream("file.csv");
+var request = http.get("http://financials.morningstar.com/ajax/exportKR2CSV.html?t=FB", function(response) {
+  response.pipe(file);
+  file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+       var stats = fs.statSync("file.csv")
+ var fileSizeInBytes = stats["size"]
+       response.write('filesize: '+fileSizeInBytes+' \n');
+    });
+
+
+
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
+    
     res.end('<h1>Hello Code and Azure Web Appssss!</h1>');
-}).listen(process.env.PORT);
+}).listen(process.env.PORT)});
